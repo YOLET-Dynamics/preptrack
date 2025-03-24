bad536fff11f3cf390c9128c0cebddcd24af293a
yonit1dev <yonit.anosie@gmail.com>
2025-03-24 10:14:41 +0300

    Feat: Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Raleway, Funnel_Display } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-raleway",
+});
+
+const funnelDisplay = Funnel_Display({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-funnel-display",
+});
+
+export const metadata: Metadata = {
+  title: "Coming Soon | preptrack.app",
+  description: "preptrack.app - Coming Soon",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.variable} ${funnelDisplay.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
